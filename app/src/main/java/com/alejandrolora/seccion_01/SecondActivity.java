80ac69a1f8081d8162995f6eda56d5934c72e0f2
JDGamingPS <gamingprm1@gmail.com>
2021-11-12 23:55:55 -0400

    repositorio Actualizado
    actualicé el repositorio de Android a la ultima versión 11/12/2021

--- app/src/main/java/com/alejandrolora/seccion_01/SecondActivity.java
+++ app/src/main/java/com/alejandrolora/seccion_01/SecondActivity.java
@@ -1,12 +1,13 @@
 package com.alejandrolora.seccion_01;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
--- app/src/main/java/com/alejandrolora/seccion_01/ThirdActivity.java
+++ app/src/main/java/com/alejandrolora/seccion_01/ThirdActivity.java
@@ -7,14 +7,15 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class ThirdActivity extends AppCompatActivity {
 
